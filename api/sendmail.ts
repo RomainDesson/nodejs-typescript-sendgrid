69381f3531865edb8244a844122ba83c7ddd4185
romaindesson <romain.desson.02320@gmail.com>
2023-12-12 08:48:00 +0100

    fix cors error

--- api/sendmail.ts
+++ api/sendmail.ts
@@ -4,6 +4,14 @@ import sendgrid from '@sendgrid/mail';
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY || '');
 
 export default async function(req: VercelRequest, res: VercelResponse) {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Autorise toutes les origines
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     const subject = req.body.subject;
     const text = req.body.text;
     const toEmail = 'romain.desson.02320@gmail.com';
