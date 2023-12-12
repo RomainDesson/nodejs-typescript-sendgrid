0684eb08575423204caedfae581be2555a293116
romaindesson <romain.desson.02320@gmail.com>
2023-12-12 08:37:33 +0100

    Refact project organization

--- api/sendmail.ts
+++ api/sendmail.ts
@@ -1,18 +1,13 @@
-require('dotenv').config();
-import express, { Request, Response } from 'express';
-import cors from 'cors';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import sendgrid from '@sendgrid/mail';
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY || '');
-const app = express();
-app.use(cors());
-app.use(express.json());
-const port = Number(process.env.PORT);
 
-app.post('/sendmail', async (req: Request, res: Response) => {
+export default async function(req: VercelRequest, res: VercelResponse) {
     const subject = req.body.subject;
     const text = req.body.text;
     const toEmail = 'romain.desson.02320@gmail.com';
+
     if (!subject || !text) {
         return res.status(400).send('Paramètres manquants');
     }
@@ -29,8 +24,4 @@ app.post('/sendmail', async (req: Request, res: Response) => {
         console.error(error);
         res.status(500).send('Erreur lors de l\'envoi de l\'email');
     }
-});
-
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Serveur démarré sur le port ${port}`);
-});
+}
